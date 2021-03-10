b9f17239e1e20f779a3f64cbd32402268636b409
Hariom868 <54442410+Hariom868@users.noreply.github.com>
2021-03-10 21:37:58 +0530

    Update create_table.js

--- create_table.js
+++ create_table.js
@@ -1,18 +1,18 @@
 const express = require('express');
 var sql = require('mysql');
 
-var connection = sql.createConnection({
+var con = sql.createConnection({
   host: 'localhost',
   user: 'root',
   password: 'Hariom123@',
   database: 'ngo'
 });
 
-conection.connect(function(err) {
+con.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
-  var sql = "CREATE TABLE Usertable (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255), address VARCHAR(255))";
-  connection.query(sql, function (err, result) {
+  var sql = "CREATE TABLE Usertable (id INT AUTO_INCREMENT PRIMARY KEY, fname VARCHAR(255),lname VARCHAR(255),email VARCHAR(255), login_type VARCHAR(255),password VARCHAR(255), address VARCHAR(255),RegDate timestamp NULL DEFAULT current_timestamp())";
+  con.query(sql, function (err, result) {
     if (err) throw err;
     console.log("Table created");
 });
