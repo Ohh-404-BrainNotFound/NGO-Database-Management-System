9fe9b1c08d7007a815531b449fb151808f07740e
Naman-1234 <namankalrabhiwani54@gmail.com>
2021-03-15 22:36:28 +0530

    Corrected Schema and Made js file for Insertion

--- routes/insertTable.js
+++ routes/insertTable.js
@@ -18,8 +18,20 @@ console.log(data);
 await con.connect(async (err)=>{
     if(err)
     throw err;
+    var countRows='SELECT COUNT(*) from users';
+    // var rows=await con.query(countRows,(err,result)=>{
+    //     if(err)
+    //     throw err;
+    //     console.log('Counting Query Successfully');
+    // })
+    var rows=5;
     console.log('Reached at starting of Insertion')
-    // var sqlQuery='INSERT INTO user VALUES()'
+    var sqlQuery=`INSERT INTO user VALUES(${rows+1},${data.fname},${data.lname},${data.email},${data.login_type},${data.pass},${data.address},${data.date},${data.number})`;
+    await con.query(sqlQuery,(err,result)=>{
+        if(err)
+        throw err;
+        console.log('Data Inserted Successfully');
+    })
 });
 });
 module.exports=router;
