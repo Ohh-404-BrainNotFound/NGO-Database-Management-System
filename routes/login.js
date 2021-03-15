f2edc084ae8bc2fff64631d1af8843f072c828b0
Naman-1234 <namankalrabhiwani54@gmail.com>
2021-03-16 02:52:06 +0530

    Sign in code working

--- routes/login.js
+++ routes/login.js
@@ -1,9 +1,30 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-
+var mysql = require("mysql");
+require("dotenv").config();
+var con = mysql.createConnection({
+  host:'localhost',
+        user:process.env.user,
+        password:process.env.password,
+        port:3306
+});
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.render('login',{});
+router.post("/", async (req, res, next) => {
+  var data = req.body;
+  console.log("login",data);
+  // await con.connect(async (err) => {
+  //   if (err) throw err;
+
+    
+    var query = `SELECT COUNT(*) from ngo.user where ngo.user.fname='${data.username}' and ngo.user.password='${data.password}'`;
+    await con.query(query, (err, result) => {
+      if (err) throw err;
+      console.log("Login", result);
+    });
+  });
+// });
+router.get("/", function (req, res, next) {
+  res.render("login", {});
 });
 
 module.exports = router;
