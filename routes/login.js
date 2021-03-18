8b73b6458ab276ac61c8be54b54d331708fc844a
nlok5923 <nlokhande5923@gmail.com>
2021-03-18 23:07:53 +0530

    working on json web token

--- routes/login.js
+++ routes/login.js
@@ -2,6 +2,18 @@ var express = require("express");
 var router = express.Router();
 var mysql = require("mysql");
 require("dotenv").config();
+const jwt = require("jsonwebtoken")
+const bcrypt = require("bcrypt")
+const {ensureGuest,ensureAuth} = require("../middleware/authMiddleware");
+
+const MAXAGE = 3*60*60*24;
+
+const getToken = (id)=>{
+    return jwt.sign({id},process.env.authpassword,{
+        expiresIn:MAXAGE
+    })
+}
+
 var con = mysql.createConnection({
   host:'localhost',
         user:process.env.user,
@@ -18,13 +30,27 @@ router.post("/", async (req, res, next) => {
     
     var query = `SELECT COUNT(*) from ngo.user where ngo.user.fname='${data.username}' and ngo.user.password='${data.password}'`;
     await con.query(query, (err, result) => {
+      // if user is correct 
+      const token = getToken(data[0]._id);
+            res.cookie("jwt",token,{
+                httpOnly:true,
+                maxAge:MAXAGE*1000
+            });
+            res.redirect("");
+      
       if (err) throw err;
       console.log("Login", result);
     });
   });
 // });
-router.get("/", function (req, res, next) {
+router.get("/", ,ensureGuest, function (req, res, next) {
   res.render("login", {});
 });
+
+
+router.get("/logout",ensureAuth,(req,res)=>{
+  res.cookie("jwt","",{maxAge:1});
+  res.redirect("/");
+})
 
 module.exports = router;
