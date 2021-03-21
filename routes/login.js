3f319a9388b98ba001538d1ee2f787e45f1eface
nlok5923 <nlokhande5923@gmail.com>
2021-03-21 11:27:47 +0530

    fix

--- routes/login.js
+++ routes/login.js
@@ -43,7 +43,7 @@ router.post("/", async (req, res, next) => {
     });
   });
 // });
-router.get("/", ,ensureGuest, function (req, res, next) {
+router.get("/",ensureGuest, function (req, res, next) {
   res.render("login", {});
 });
 
