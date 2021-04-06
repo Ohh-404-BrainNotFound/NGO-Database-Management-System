0b8f28e276987cc750fd7c1f80892f35763f1455
Hariom868 <54442410+Hariom868@users.noreply.github.com>
2021-04-07 03:15:12 +0530

    Update ngologin.js

--- ngologin.js
+++ ngologin.js
@@ -1,44 +1,23 @@
-var express = require('express');
-var router = express.Router();
-var connection  = require('../lib/db');// inside require put the address of db.js
-// we have used two time because in the first it shows garbage value
-router.get('/', function(req, res, next){    
-res.render('auth/login', {
-email: '',
-password: ''     
+let express = require('express');
+let router = express.Router();
+let db=require('../lib/db');//where you have stored the db.js
+//GET users listing. 
+router.get('/login', function(req, res, next) {
+  res.render('login-form');
+});
+router.post('/login', function(req, res){
+    let emailAddress = req.body.email_address;
+    let password = req.body.password;
+    let sql='SELECT * FROM ngosignup WHERE email_address =? AND password =?';
+    db.query(sql, [emailAddress, password], function (err, data, fields) {
+        if(err) throw err
+        if(data.length>0){
+            req.session.loggedinUser= true;
+            req.session.emailAddress= emailAddress;
+            res.redirect('/dashboard');
+        }else{
+            res.render('login-form',{alertMsg:"Your Email Address or password is wrong"});
+        }
+    })
 })
-})
-// to get the value of email id from the login
-router.get('/login', function(req, res, next){    
-res.render('auth/login', {
-email: '',
-password: ''    
-})
-})
-//we are checking the value and matching it with our database
-router.post('/authentication', function(req, res, next) {
-var email = req.body.email;
-var password = req.body.password;
-connection.query('SELECT * FROM ngosignup WHERE email = ? AND password = ?', [email, password], function(err, rows, fields) {
-if(err) throw err
-if (rows.length <= 0) {
-req.flash('error', 'Please correct enter email and Password!')
-res.redirect('/login')
-}
-else { 
-req.session.loggedin = true;
-req.session.email = email;
-res.redirect('/home');
-}            
-})
-})
-router.get('/home', function(req, res, next) {
-    if (req.session.loggedin) {
-    res.render('auth/home', {
-    email: req.session.email,     
-    });
-    } else {
-    req.flash('success', 'Please login first!');
-    res.redirect('/login');
-    }
-    });
+module.exports = router;
