6a19dfe7f424412d1edcf34445d6625f959b4971
nlok5923 <nlokhande5923@gmail.com>
2021-04-06 15:49:56 +0530

    added registration feature for NGo

--- routes/ngo-signup.js
+++ routes/ngo-signup.js
@@ -1,10 +1,22 @@
 var express = require('express');
 var router = express.Router();
-var multer = require('multer');
+// var multer = require('multer');
+const mysql = require('mysql');
+require('dotenv').config();
 
+const connection = mysql.createConnection({
+        host: 'localhost',
+        user: process.env.user,
+        password: process.env.password,
+        port: 3306
+      }
+)
+
+//we will use below code for uploading image feature
+/*
 var storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null,"/public/uploads")      //you tell where to upload the files,
+      cb(null, __dirname + "/public/uploads")      //you tell where to upload the files,
     },
     filename: function (req, file, cb) {
     //   var regex = new RegExp('[^.]+$');
@@ -17,9 +29,20 @@ var storage = multer.diskStorage({
 var upload = multer({
     storage: storage,
   });
+*/
 
-router.post('/', upload.single('ngologo'), function(req, res, next) {
-    console.log(req.file);
+router.post('/', async function(req, res, next) {
+  var data = req.body;
+  console.log(data);
+  console.log('Reached at starting of Insertion')
+  var sqlQuery = `INSERT INTO ngo.ngodata (ngo_name,ngo_mail,ngo_password,ngo_info,government_id,ngo_address,ngo_bank,ngo_account,ngo_ifsccode) VALUES('${data.ngo_name}','${data.ngo_email}','${data.ngo_pass}','${data.ngo_information}','${data.ngo_id}','${data.address}','${data.bank_name}','${data.account_no}','${data.ifsc_code}')`;
+  console.log("Query", sqlQuery);
+  await connection.query(sqlQuery, (err, result) => {
+    if (err)
+    throw err;
+    console.log('Data Inserted Successfully');
+  })
+  console.log(req.body);
 });
 
 module.exports = router;
--- sql_queries/query.sql
+++ sql_queries/query.sql
@@ -5,3 +5,18 @@ CREATE TABLE  images (
   file_name varchar(45) collate latin1_general_ci NOT NULL,
   PRIMARY KEY (img_id)
 );
+
+-- query for ngo sign up table
+
+create table ngoregistration (
+  ngo_mail varchar(124) primary key not null,
+  ngo_name varchar(125) not null,
+  ngo_password varchar(30) not null,
+  ngo_info varchar(500) not null,
+  government_id varchar(125) not null,
+  ngo_address varchar(200) not null,
+  ngo_bank varchar(125) not null,
+  ngo_account int(30) not null,
+  ngo_ifsccode varchar(30) not null,
+  regdate timestamp NULL DEFAULT CURRENT_TIMESTAMP
+);
