dd27b91cf25c80539c56b5083058959edd6c99da
nlok5923 <nlokhande5923@gmail.com>
2021-04-07 09:13:46 +0530

    test

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:vue/essential"
+    ],
+    "parserOptions": {
+        "ecmaVersion": 12
+    },
+    "plugins": [
+        "vue"
+    ],
+    "rules": {
+    }
+};
