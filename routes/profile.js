031d7f540d1667a74b00d6e5eb4339cb9706778f
nlok5923 <nlokhande5923@gmail.com>
2021-04-15 16:39:46 +0530

    make top donor dynamic

--- routes/profile.js
+++ routes/profile.js
@@ -1,7 +1,5 @@
 var express = require('express');
 var router = express.Router();
-const { query } = require('express');
-const mysql = require('mysql');
 require('dotenv').config();
 const { executeAndReturn, executeQuery } = require('../controllers/connection');
 const utils = require('../controllers/utls');
@@ -15,26 +13,38 @@ router.get('/', async function(req, res, next) {
   await executeAndReturn(query)
   .then((result) => {
     const userInfo = result[0];
+    console.log(userInfo);
     res.render('./dashboard/profile',{userInfo});
   })
 });
 
 router.post('/',upload.single('userimage') , async (req,res)=> {
-  console.log(req.file);
   const {fname, lname, email, password, phoneNumber, address} = req.body;
-  let query = `update ngo.user set fname = "${fname}"   lname = "${lname}"   password = "${password}"  phoneNumber = "${phoneNumber}"  address = "${address}" where email = "${req.session.userEmail}" `
-  await executeQuery(query);
-  let userImage = {
-    img: fs.readFileSync(req.file.path),
-    file_name: req.file.filename
-  };
-  var imgQuery = `update ngo.user SET image = "${userImage.file_name}" where email = "${req.session.userEmail}" `
-  await execute(imgQuery);
+  let query = `update ngo.user set fname = "${fname}" , lname = "${lname}"  , password = "${password}" , phoneNumber = "${phoneNumber}" , address = "${address}" where email = "${req.session.userEmail}" `
+  await executeQuery(query)
+  .then(async () => {
+    if(req.file) {
+      let userImage = {
+        img: fs.readFileSync(req.file.path),
+        file_name: req.file.filename
+      };
+      var imgQuery = `update ngo.user SET image = "${userImage.file_name}" where email = "${req.session.userEmail}" `
+      await executeQuery(imgQuery)
+      .then(() => {
+        res.redirect('/dashboard-user');
+      })
+    }else {
+      res.redirect('/dashboard-user');
+    }
+  })
 })
 
 router.post('/delete', async (req, res) => {
   let deleteQuery = `delete from ngo.user where email = "${req.body.userEmail}" `;
-  await executeQuery(deleteQuery);
+  await executeQuery(deleteQuery)
+  .then(() => {
+    res.redirect('/dashboard-user');
+  })
 })
 
 module.exports = router;
--- routes/topdonor.js
+++ routes/topdonor.js
@@ -2,12 +2,17 @@ const express = require('express');
 const router = express.Router();
 const { executeAndReturn } = require('../controllers/connection');
 
-router.get('/', function(req, res, next) {
+router.get('/', async function(req, res, next) {
 //for actual use
 //const ngoEmail = req.session.ngoEmail;
 //for testing purpose
-const ngoEmail = '12@gmail.com';
-  res.render('./dashboard/topdonor',{});
+  const ngoEmail = '12@gmail.com';
+  let getMaxDonors = ` SELECT donor_name , sum(amount) as total_donation from ngo.ngo_donor_record group by donor_name order by sum(amount) desc limit 3; `;
+  await executeAndReturn(getMaxDonors)
+  .then((data) => {
+    console.log(data);
+    res.render('./dashboard/topdonor',{donorData: data});
+  })
 });
 
 module.exports = router;
